d2fe0d9a0bc0c4f0d63f45c8068adc8e8031de6d
Thor <thor@3--t.in>
2025-07-15 18:39:07 +0200

    basic scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './assets/css/colors.css'
-import './assets/css/fonts.css'
-import './assets/css/index.css'
 import './assets/css/tailwind.css'
-import './assets/css/widths.css'
-import './assets/css/components/darkmode-button.css'
-import './assets/css/components/seperator.css'
+import './assets/css/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
